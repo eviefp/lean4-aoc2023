4dc57ee8e06fa740e230fd5d840673d794ec42b7
Evie Ciobanu <me@eevie.ro>
2023-12-07 16:36:37 +0200

    day7

--- AdventOfCode/All.lean
+++ AdventOfCode/All.lean
@@ -4,6 +4,7 @@ import AdventOfCode.Day3
 import AdventOfCode.Day4
 import AdventOfCode.Day5
 import AdventOfCode.Day6
+import AdventOfCode.Day7
 
 export AdventOfCode.Day1 (main)
 export AdventOfCode.Day2 (main)
@@ -11,3 +12,4 @@ export AdventOfCode.Day3 (main)
 export AdventOfCode.Day4 (main)
 export AdventOfCode.Day5 (main)
 export AdventOfCode.Day6 (main)
+export AdventOfCode.Day7 (main)
