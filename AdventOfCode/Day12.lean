b66eb24486be74d7c839944fc105a0db726aead1
Evie Ciobanu <me@eevie.ro>
2023-12-13 01:33:56 +0200

    day12 with StateTRef in 600ms

--- AdventOfCode/Day12.lean
+++ AdventOfCode/Day12.lean
@@ -18,7 +18,7 @@ inductive Spring where
   | Operational
   | Damaged
   | Unknown
-deriving BEq
+deriving BEq, Hashable
 
 instance: ToString Spring where
   toString
@@ -29,7 +29,7 @@ instance: ToString Spring where
 structure Input where
   record: List Spring
   series: List Nat
-deriving Inhabited
+deriving Inhabited, BEq, Hashable
 
 instance: ToString Input where
   toString i := String.join (i.record.map ToString.toString) ++ " " ++ i.series.toString
@@ -38,11 +38,11 @@ def countMaxDamaged: List Spring -> Nat :=
   List.length
     ∘ List.filter (not ∘ (BEq.beq Spring.Operational))
 
-def numberOfVariants: List Spring -> List Nat -> Bool -> Nat
+def numberOfVariants: List Spring -> List Nat -> Bool -> StateRefT (Lean.HashMap Input Nat) IO  Nat
   | Spring.Operational :: xs, 0 :: ys, true => numberOfVariants xs ys false -- skip the zero
   | Spring.Operational :: xs, ys, false => numberOfVariants xs ys false -- skipping along
-  | Spring.Operational :: _, _, true => 0 -- if we were inside a group but got a wrong entry, stop
-  | Spring.Damaged :: _, 0 :: _, _ => 0
+  | Spring.Operational :: _, _, true => pure 0 -- if we were inside a group but got a wrong entry, stop
+  | Spring.Damaged :: _, 0 :: _, _ => pure 0
   | Spring.Damaged :: xs, (k + 1) :: ys, _ => numberOfVariants xs (k :: ys) true -- we have to start/continue along
   | Spring.Unknown :: xs, [], false => numberOfVariants xs [] false -- fill in with operationals if we're done counting
   | Spring.Unknown :: xs, 0 :: ys, _ => numberOfVariants xs ys false -- just done with a group so go back to outside a group, must use operational
@@ -51,18 +51,26 @@ def numberOfVariants: List Spring -> List Nat -> Bool -> Nat
       let maxDamaged := countMaxDamaged xs
       let neededDamaged := Monoid.Nat.Sum.Instance.fold (k :: ys)
       if maxDamaged < neededDamaged
-        then 0 -- impossible to fulfil, don't diverge
-        else
-            Nat.add
-                (numberOfVariants xs ((k + 1) :: ys) false) -- use operational
-                (numberOfVariants xs ((k) :: ys) true)  -- use broken
-  | [], [0], _ => 1
-  | [], [], _ => 1
-  | _, _, _ => 0
+        then pure 0 -- impossible to fulfil, don't diverge
+        else do
+          let cached <- flip Lean.HashMap.find? ⟨Spring.Unknown :: xs, (k+1) :: ys⟩ <$> get
+          match cached with
+            | .some result => pure result
+            | .none => do
+                let operational <- numberOfVariants xs ((k + 1) :: ys) false
+                let broken <- (numberOfVariants xs ((k) :: ys) true)
+                modify (λhm => hm.insert ⟨Spring.Unknown :: xs, (k+1) :: ys⟩ (operational + broken))
+                pure $ operational + broken
+  | [], [0], _ => pure 1
+  | [], [], _ => pure 1
+  | _, _, _ => pure 0
 
-def solve: List Input -> Nat :=
-  Monoid.Nat.Sum.Instance.foldMap
-    (λ i => numberOfVariants i.record i.series false)
+def solve: List Input -> IO Nat :=
+  Monoid.Nat.Sum.Instance.foldMapM
+    (λ i =>
+      let state := numberOfVariants i.record i.series false
+      state.run' Lean.HashMap.empty
+    )
 
 def updateInput (input: Input): Input :=
   { record := List.intercalate [ Spring.Unknown ] $ List.replicate 5 input.record
@@ -119,8 +127,10 @@ def main: IO Unit := do
       { toString := "./data/day12-1" }
       Parser.input
 
-  IO.println s!"part1 {solve parsed1}"
+  let result1 <- solve parsed1
+  IO.println s!"part1 {result1}"
 
-  let input2 := Sample.input1.map updateInput
+  let input2 := parsed1.map updateInput
 
-  IO.println s!"part2 {solve input2}"
+  let result2 <- solve input2
+  IO.println s!"part2 {result2}"
